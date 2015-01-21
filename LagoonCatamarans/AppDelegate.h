e7af3173c93686cba66fc44f84193c7e37331ed2
Shoaib Khan <saam.u.dev@gmail.com>
2015-01-21 20:30:22 +0500

    Initial Commit

---
+++ LagoonCatamarans/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LagoonCatamarans
+//
+//  Created by Yosmite on 1/21/15.
+//  Copyright (c) 2015 LC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LagoonCatamarans/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LagoonCatamarans
+//
+//  Created by Yosmite on 1/21/15.
+//  Copyright (c) 2015 LC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
